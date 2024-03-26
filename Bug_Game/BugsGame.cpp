1f24cc2442c1deb81bc7ec0a0106c22c529b805a
tbvjchvkfl <137769043+tbvjchvkfl@users.noreply.github.com>
2024-03-26 23:24:01 +0900

    UI & 게임 로직 작업

--- Bug_Game/BugsGame.cpp
+++ Bug_Game/BugsGame.cpp
@@ -9,12 +9,18 @@ HRESULT BugsGame::Initialize(HINSTANCE hinstance, LPCWSTR title, UINT width, UIN
     ThrowIfFailed(hr, "Failed to D2DFramework::Initialize()");
 
     mspBackground = std::make_shared<Actor>(this, L"Images/back.png", 0.0f, 0.0f, 1.0f);
+    mspUI = std::make_shared<UI_Box>(this);
+    mspSubUI = std::make_shared<Actor>(this, L"Images/Sub_UI.png", 0.0f, 250.0f, 1.0f);
     mspPlayer = std::make_shared<Player>(this);
     for (int i = 0; i < 40; i++)
     {
         mBuglist.push_back(std::make_shared<Bug>(this, mspPlayer.get()));
     }
     mStage = 0;
+    mGameStart = false;
+    mGameClear = false;
+    time = 0;
+    
     return S_OK;
 }
 
@@ -24,22 +30,94 @@ void BugsGame::Render()
     mspRenderTarget->SetTransform(D2D1::Matrix3x2F::Identity());
     mspRenderTarget->Clear(D2D1::ColorF(0.0f, 0.2f, 0.4f, 1.0f));
     
-    CheckBugs();
-    CheckCollision();
-
-    mspBackground->Draw();
-    
-    WriteText(L"Testing Text", 100, 100, 150, 150, L"¸¼Àº°íµñ", 12);
-
-    if (mStage == 0)
+    //UI ·ÎÁ÷
+    if (!mspUI->mNextStep)
     {
-        for (auto& bug : mBuglist)
+        mspBackground->Draw();
+        mspUI->Draw();
+
+        if (GetAsyncKeyState(VK_LBUTTON) & 0x8000)
         {
-            bug->Draw();
+            POINT pt;
+            GetCursorPos(&pt);
+            ScreenToClient(mHwnd, &pt);
+            if (mspUI->IsClicked(pt))
+            {
+                mspUI->mNextStep = true;
+            }
         }
 
+        mspSubUI->Draw();
+        WriteText(L"¹æÇâÅ°¸¦ ÀÌ¿ëÇÏ¿© ¹ú·¹ Ä³¸¯ÅÍ¸¦ ¿òÁ÷¿© ´Ù¸¥ ¹ú·¹µéÀ» Àâ¾Æ¸Ô´Â °ÔÀÓÀÔ´Ï´Ù.", 20, 280, 540, 100, L"¸¼Àº°íµñ", 25);
+        WriteText(L"Á¦ÇÑ ½Ã°£¾È¿¡ ´Ù¸¥ ¹ú·¹¸¦ ¸ðµÎ Àâ¾Æ¸ÔÀ¸¼¼¿ä.", 20, 350, 800, 100, L"¸¼Àº°íµñ", 25);
 
-        mspPlayer->Draw();
+
+        WriteText(L"GAME START", 40, 50, 800, 100, L"¸¼Àº°íµñ", 90);
+    }
+
+    // °ÔÀÓ ·ÎÁ÷
+    if (mspUI->mNextStep)
+    {
+        mspBackground->Draw();
+        if (!mGameStart)
+        {
+            if (time < 60)
+            {
+                WriteText(L"3", 480, 300, 80, 10, L"¸¼Àº°íµñ", 90);
+                time++;
+            }
+            if (time >= 60 && time < 120)
+            {
+                WriteText(L"2", 480, 300, 80, 10, L"¸¼Àº°íµñ", 90);
+                time++;
+            }
+            if (time >= 120 && time < 180)
+            {
+                WriteText(L"1", 480, 300, 80, 10, L"¸¼Àº°íµñ", 90);
+                time++;
+            }
+            if (time >= 180 && time < 240)
+            {
+                WriteText(L"Game Start!", 280, 300, 800, 10, L"¸¼Àº°íµñ", 90);
+                time++;
+            }
+            if (time >= 240)
+            {
+                mGameStart = true;
+            }
+        }
+        
+        if (mGameStart)
+        {
+            if (mStage == 0)
+            {
+                CheckCollision();
+                for (auto& bug : mBuglist)
+                {
+                    bug->Draw();
+                }
+
+                mspPlayer->Draw();
+            }
+            if (mGameClear)
+            {
+                for (int i = 0; i < 20; i++)
+                {
+                    mBuglist.push_back(std::make_shared<Bug>(this, mspPlayer.get()));
+                }
+                mGameClear = false;
+            }
+            if (mStage == 1)
+            {
+                CheckBugs();
+                for (auto& bug : mBuglist)
+                {
+                    bug->Draw();
+                }
+                mspPlayer->Draw();
+            }
+        }
+        
     }
     
     HRESULT hr = mspRenderTarget->EndDraw();
@@ -59,6 +137,8 @@ void BugsGame::Release()
 
     mBuglist.clear();
     mspPlayer.reset();
+    mspSubUI.reset();
+    mspUI.reset();
     mspBackground.reset();
 
     D2DFramework::Release();
@@ -103,17 +183,11 @@ void BugsGame::CheckCollision()
                    return p->IsCollision(PlayerPosition);
                }
            );
-           mBuglist.erase(list_iterator, mBuglist.end());
-}
+    mBuglist.erase(list_iterator, mBuglist.end());
 
-void BugsGame::Bug_Move()
-{
-    auto Pos = mspPlayer->GetPosition();
-
-    for (auto& bug : mBuglist)
+    if (mBuglist.empty())
     {
-        auto bPos = bug->GetPosition();
-        
+        mStage++;
+        mGameClear = true;
     }
 }
-
--- Bug_Game/BugsGame.h
+++ Bug_Game/BugsGame.h
@@ -1,10 +1,12 @@
 #pragma once
 #include <chrono>
 #include <list>
+#include <vector>
 #include <memory>
 #include "D2DFramework.h"
 #include "Actor.h"
 #include "Bug.h"
+#include "UI_Box.h"
 
 class BugsGame : public D2DFramework
 {
@@ -12,10 +14,15 @@ private:
 	std::list<std::shared_ptr<Bug>> mBuglist;
 	std::shared_ptr<Actor> mspBackground;
 	std::shared_ptr<Player> mspPlayer;
+	std::shared_ptr<UI_Box> mspUI;
+	std::shared_ptr<Actor> mspSubUI;
+	
 
 public:
 	int mStage;
-
+	bool mGameStart;
+	bool mGameClear;
+	int time;
 public:
 	virtual HRESULT Initialize(HINSTANCE hinstance, LPCWSTR title = L"BugsGame", UINT width = 1024, UINT height = 768) override;
 	virtual void Render() override;
@@ -23,6 +30,5 @@ public:
 
 	void CheckBugs();
 	void CheckCollision();
-	void Bug_Move();
 };
 
