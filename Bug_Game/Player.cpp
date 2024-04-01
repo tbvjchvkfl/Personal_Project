921714cd95b599a54950c0bdb78a18ec47146ea8
tbvjchvkfl <137769043+tbvjchvkfl@users.noreply.github.com>
2024-04-01 22:18:33 +0900

    이동 함수 가속도로 수정

--- Bug_Game/Player.cpp
+++ Bug_Game/Player.cpp
@@ -1,16 +1,17 @@
 #include "Player.h"
 
-
 Player::Player(D2DFramework* pFramework) : Actor(pFramework, L"Images/Player.png")
 {
     RECT rct;
     GetClientRect(pFramework->GetHWND(), &rct);
+    
+    mX = ((rct.left + rct.right) / 2.0f) - 50.0f;
+    mY = ((rct.top + rct.bottom) / 2.0f) - 50.0f;
 
-    mX = (rct.right + rct.left) / 2.0f;
-    mY = (rct.bottom + rct.top) / 2.0f;
-
-    mSpeed = 1.0f;
+    mIsDead = false;
+    mGravity = 9.3f;
     mVelocity = 1.0f;
+    mStage = 0;
 }
 
 Player::~Player()
@@ -29,6 +30,8 @@ void Player::Draw()
 
     D2D1_SIZE_U size = mpBitmap->GetPixelSize();
 
+
+
     auto matTranslate = D2D1::Matrix3x2F::Translation(mX, mY);
     pRenderTarget->SetTransform(matTranslate);
 
@@ -38,22 +41,88 @@ void Player::Draw()
 
 bool Player::input()
 {
-    // 가속도로 만들 것.
-    if (GetAsyncKeyState(VK_RIGHT))
+    if (mStage == 0)
     {
-        mX = (mX + mSpeed) * mVelocity;
+        RECT rect;
+        GetClientRect(mpFramework->GetHWND(), &rect);
+        auto Playersize = mpBitmap->GetPixelSize();
+
+        // 가속도 (틈날 때 물리도 적용시켜보자!)
+        if (GetAsyncKeyState(VK_RIGHT))
+        {
+            if (mX + Playersize.width >= rect.right)
+            {
+                mX = mX;
+            }
+            else
+            {
+                mVelocity += 0.2f;
+                mX += mVelocity / mGravity;
+            }
+        }
+        if (GetAsyncKeyState(VK_LEFT))
+        {
+            if (mX <= rect.left)
+            {
+                mX = mX;
+            }
+            else
+            {
+                mVelocity += 0.2f;
+                mX -= mVelocity / mGravity;
+            }
+        }
+        if (GetAsyncKeyState(VK_UP))
+        {
+            if (mY <= rect.top)
+            {
+                mY = mY;
+            }
+            else
+            {
+                mVelocity += 0.2f;
+                mY -= mVelocity / mGravity;
+            }
+        }
+        if (GetAsyncKeyState(VK_DOWN))
+        {
+            if (mY + Playersize.height >= rect.bottom)
+            {
+                mY = mY;
+            }
+            else
+            {
+                mVelocity += 0.2f;
+                mY += mVelocity / mGravity;
+            }
+        }
+        return false;
     }
-    if (GetAsyncKeyState(VK_LEFT))
-    {
-        mX--;
-    }
-    if (GetAsyncKeyState(VK_UP))
-    {
-        mY--;
-    }
-    if (GetAsyncKeyState(VK_DOWN))
-    {
-        mY++;
-    }
-    return false;
 }
+
+bool Player::IsCollision(D2D_VECTOR_2F& Pos, D2D1_SIZE_U& bugsize)
+{
+    auto size = mpBitmap->GetPixelSize();
+    if (bugsize.width + Pos.x <= mX || Pos.x >= mX + size.width)
+    {
+        return false;
+    }
+    if (bugsize.height + Pos.y <= mY || Pos.y >= mY + size.height)
+    {
+        return false;
+    }
+    mIsDead = true;
+    return true;
+}
+
+void Player::Reset()
+{
+    RECT rct;
+    GetClientRect(mpFramework->GetHWND(), &rct);
+
+    mX = ((rct.left + rct.right) / 2.0f) - 50.0f;
+    mY = ((rct.top + rct.bottom) / 2.0f) - 50.0f;
+
+    mIsDead = false;
+}
+
--- Bug_Game/Player.h
+++ Bug_Game/Player.h
@@ -1,18 +1,24 @@
 #pragma once
 #include "Actor.h"
+
 class Player : public Actor
 {
 private:
 	const D2D_POINT_2F UPVECTOR{ 0.0f, 0.0f };
 
 public:
-	float mSpeed;
+	float mGravity;
 	float mVelocity;
+	bool mIsDead;
+	int mStage;
 
 public:
 	Player(D2DFramework* pFramework);
 	~Player();
 	virtual void Draw() override;
 	bool input();
+
+	bool IsCollision(D2D_VECTOR_2F& Pos, D2D1_SIZE_U& bugsize);
+	void Reset();
 };
 
