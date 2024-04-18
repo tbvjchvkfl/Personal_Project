cf7dccbd041be106a856c311d7ebe66a1abb3a81
tbvjchvkfl <137769043+tbvjchvkfl@users.noreply.github.com>
2024-04-18 23:43:24 +0900

    Add to Projectile Header File & CPP

---
+++ UE5_HorrorGame/Source/UE5_Horrors_Game/Projectile.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Projectile.generated.h"
+
+UCLASS()
+class UE5_HORRORS_GAME_API AProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AProjectile();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+protected:
+	
+	UPROPERTY(EditAnywhere, Category = "Collsion")
+	class UStaticMeshComponent* ProjectileMesh;
+	UPROPERTY(EditAnywhere, Category = "Collsion")
+	class UProjectileMovementComponent* ProjectileMovement;
+
+public:
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+};
