8ee6908a9554d7e2cdbdd0bbd790edac5ab18e92
tbvjchvkfl <137769043+tbvjchvkfl@users.noreply.github.com>
2024-04-22 23:23:04 +0900

    Add to BehaviorTreeCustomNode

---
+++ UE5_HorrorGame/Source/UE5_Horrors_Game/BTTask_FindRandomLocation.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTTask_FindRandomLocation.h"
+#include "AI_Controller.h"
+#include "EnemyCharacter.h"
+#include "NavigationSystem.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+UBTTask_FindRandomLocation::UBTTask_FindRandomLocation(FObjectInitializer const& ObjectInitializer)
+{
+	NodeName = "Find Random Location In NavMeesh";
+
+}
+
+EBTNodeResult::Type UBTTask_FindRandomLocation::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	if (AAI_Controller* const Controll = Cast<AAI_Controller>(OwnerComp.GetAIOwner()))
+	{
+		if (auto* const NPC = Controll->GetPawn())
+		{
+			FVector const Origin = NPC->GetActorLocation();
+
+			if (auto* const NavSys = UNavigationSystemV1::GetCurrent(GetWorld()))
+			{
+				FNavLocation Loc;
+				if (NavSys->GetRandomPointInNavigableRadius(Origin, SearchRadius, Loc))
+				{
+					OwnerComp.GetBlackboardComponent()->SetValueAsVector(GetSelectedBlackboardKey(), Loc.Location);
+				}
+				FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+				return EBTNodeResult::Succeeded;
+			}
+		}
+	}
+	return EBTNodeResult::Failed;
+}
