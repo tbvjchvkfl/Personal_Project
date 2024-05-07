9d4290296b8200f83f07e2e2a5c56c4284ba72b8
tbvjchvkfl <137769043+tbvjchvkfl@users.noreply.github.com>
2024-05-07 22:53:50 +0900

    필요없어진 소스 파일 삭제

--- UE5_HorrorGame/Source/UE5_Horrors_Game/UE5_Horrors_Game.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "UE5_Horrors_Game.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE5_Horrors_Game, "UE5_Horrors_Game" );
