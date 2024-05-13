8a9e2a5c928c4b04e713ae95f3db193271499c1e
tbvjchvkfl <137769043+tbvjchvkfl@users.noreply.github.com>
2024-05-13 20:11:00 +0900

    재화와 아이템 표시 분리

--- UE5_HorrorGame/Source/UE5_HorrorsGame/Private/UI/Inventory.cpp
+++ UE5_HorrorGame/Source/UE5_HorrorsGame/Private/UI/Inventory.cpp
@@ -6,23 +6,27 @@
 #include "Character/Player/PlayerCharacter.h"
 #include "UI/InventorySlot.h"
 #include "Components/WrapBox.h"
+#include "Components/TextBlock.h"
+#include "UI/HorrorsHUD.h"
+#include "Data/ItemStruct.h"
+#include "Object/Item/PickUpItem.h"
 
-// Add to Viewport 할 때 불리는 함수
+
+// Add to Viewport 할 때 불리는 함수`
 void UInventory::NativeConstruct()
 {
 	Super::NativeConstruct();
-
-	
+	Player = Cast<APlayerCharacter>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
+	JustSetCoinData = true;
+	CoinText->SetText(FText::AsNumber(Player->GetPlayerCoin()));
 }
 
 void UInventory::AddItemToInventory()
 {
-	Player = Cast<APlayerCharacter>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
-
 	if (Player)
 	{
 		InventoryItems = Player->GetInventoryItem();
-
+		BindingCoinText();
 		for (int i = 0; i < InventoryItems.Num(); i++)
 		{
 			if (InventoryClass)
@@ -31,23 +35,18 @@ void UInventory::AddItemToInventory()
 				if (SlotWidget)
 				{
 					SlotWidget->SetItemSlot(InventoryItems[i]);
-					if (Player->EnableAddItem)
-					{
-						WrapBox->AddChild(SlotWidget);
-					}
+					WrapBox->AddChild(SlotWidget);
 				}
 			}
 		}
 	}
 }
 
-void UInventory::AddToSlot()
+void UInventory::BindingCoinText()
 {
-	if (InventoryClass)
-	{
-		if (SlotWidget)
-		{
-			WrapBox->AddChild(SlotWidget);
-		}
-	}
+	FString CoinString = FString::Printf(TEXT("%d"), Player->GetPlayerCoin());
+	CoinText->SetText(FText::FromString(CoinString));
 }
+
+
+
