42ee245e1f2318000f34b187c0b04d55dba1bcec
tbvjchvkfl <137769043+tbvjchvkfl@users.noreply.github.com>
2024-05-17 01:53:29 +0900

    AI C++ 노드 추가

---
+++ UE5_HorrorGame/Source/UE5_HorrorsGame/Public/AI/BT/BTService_IsPlayerInMeleeRange.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Services/BTService_BlackboardBase.h"
+#include "BTService_IsPlayerInMeleeRange.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_HORRORSGAME_API UBTService_IsPlayerInMeleeRange : public UBTService_BlackboardBase
+{
+	GENERATED_BODY()
+	
+public:
+	UBTService_IsPlayerInMeleeRange();
+	virtual void OnBecomeRelevant(UBehaviorTreeComponent &OwnerComp, uint8 *NodeMemory) override;
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI", meta = (AllowPrivateAccess = "true"))
+	float MeleeRange = 2.5f;
+};
---
+++ UE5_HorrorGame/Source/UE5_HorrorsGame/Public/AI/BT/BTTask_MeleeAttack.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+#include "BTTask_MeleeAttack.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_HORRORSGAME_API UBTTask_MeleeAttack : public UBTTask_BlackboardBase
+{
+	GENERATED_BODY()
+	
+};
