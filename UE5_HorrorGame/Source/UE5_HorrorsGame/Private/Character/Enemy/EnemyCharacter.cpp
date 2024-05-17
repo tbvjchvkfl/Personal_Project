33f5e9939f32a29b0020a9f27117295173bcb6e3
tbvjchvkfl <137769043+tbvjchvkfl@users.noreply.github.com>
2024-05-18 04:00:20 +0900

    공격 몽타주 & 포효 몽타주 추가 비헤이비어 트리& 블랙보드 적용

--- UE5_HorrorGame/Source/UE5_HorrorsGame/Private/Character/Enemy/EnemyCharacter.cpp
+++ UE5_HorrorGame/Source/UE5_HorrorsGame/Private/Character/Enemy/EnemyCharacter.cpp
@@ -12,3 +12,31 @@ UBehaviorTree *AEnemyCharacter::GetBehaviorTree() const
 {
 	return Tree;
 }
+
+UAnimMontage *AEnemyCharacter::GetScreamMontage() const
+{
+	return ScreamMontage;
+}
+
+UAnimMontage *AEnemyCharacter::GetAttackMontage() const
+{
+	return AttackMontage;
+}
+
+int AEnemyCharacter::MeleeAttack_Implementation()
+{
+	if (AttackMontage)
+	{
+		PlayAnimMontage(AttackMontage);
+	}
+	return 0;
+}
+
+int AEnemyCharacter::ScreamAction_Implementation()
+{
+	if (ScreamMontage)
+	{
+		PlayAnimMontage(ScreamMontage);
+	}
+	return 0;
+}
