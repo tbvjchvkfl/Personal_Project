40d87b98c5362c54207320e4fb8a28bbfaae9d98
tbvjchvkfl <137769043+tbvjchvkfl@users.noreply.github.com>
2024-05-19 21:41:21 +0900

    콜리젼 관리를 위한 노티파이 스테이트 추가

---
+++ UE5_HorrorGame/Source/UE5_HorrorsGame/Public/Anim/NotifyState/AttackAnimNotify.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "AttackAnimNotify.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_HORRORSGAME_API UAttackAnimNotify : public UAnimNotify
+{
+	GENERATED_BODY()
+	
+};
