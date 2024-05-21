527191a3adffb35a6c203c083cbf0624cb78cf8c
tbvjchvkfl <137769043+tbvjchvkfl@users.noreply.github.com>
2024-05-21 22:05:59 +0900

    사망 모션 추가 및 플레이어와 인터렉션 추가

--- UE5_HorrorGame/Source/UE5_HorrorsGame/Private/Character/Enemy/EnemyCharacter.cpp
+++ UE5_HorrorGame/Source/UE5_HorrorsGame/Private/Character/Enemy/EnemyCharacter.cpp
@@ -3,9 +3,17 @@
 
 #include "Character/Enemy/EnemyCharacter.h"
 #include "BehaviorTree/BehaviorTree.h"
+#include "Components/BoxComponent.h"
+#include "Character/Player/PlayerCharacter.h"
+#include "Kismet/GameplayStatics.h"
 
 AEnemyCharacter::AEnemyCharacter()
 {
+	PrimaryActorTick.bCanEverTick = true;
+
+	HandCollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("CollsionBox"));
+	HandCollisionBox->SetupAttachment(GetMesh(), "HandR_Socket");
+	
 }
 
 UBehaviorTree *AEnemyCharacter::GetBehaviorTree() const
@@ -39,4 +47,51 @@ int AEnemyCharacter::ScreamAction_Implementation()
 		PlayAnimMontage(ScreamMontage);
 	}
 	return 0;
+}
+
+void AEnemyCharacter::AttackStart() const
+{
+	HandCollisionBox->SetCollisionProfileName("Fist");
+	HandCollisionBox->SetNotifyRigidBodyCollision(true);
+	HandCollisionBox->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+}
+
+void AEnemyCharacter::AttackEnd() const
+{
+	HandCollisionBox->SetCollisionProfileName("Fist");
+	HandCollisionBox->SetNotifyRigidBodyCollision(false);
+	HandCollisionBox->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
+void AEnemyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	HandCollisionBox->OnComponentBeginOverlap.AddDynamic(this, &AEnemyCharacter::OnAttackOverlapBegin);
+	HandCollisionBox->OnComponentEndOverlap.AddDynamic(this, &AEnemyCharacter::OnAttackOverlapEnd);
+}
+
+void AEnemyCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AEnemyCharacter::OnAttackOverlapBegin(UPrimitiveComponent *const OverlapComp, AActor *const OtherActor, UPrimitiveComponent *const OtherComponent, int const OtherBodyIndex, bool const FromSweep, FHitResult const &SweepResult)
+{
+	if (OtherActor == this)
+	{
+		return;
+	}
+	if (auto const Enemy = Cast<APlayerCharacter>(OtherActor))
+	{
+		UGameplayStatics::ApplyDamage(Enemy, 30.0f, NULL, this, UDamageType::StaticClass());
+		
+		GEngine->AddOnScreenDebugMessage(-1, 3, FColor::Red, FString::Printf(TEXT("Enemy Is Attack")), true);
+	}
+}
+
+void AEnemyCharacter::OnAttackOverlapEnd(UPrimitiveComponent *const OverlapComp, AActor *const OtherActor, UPrimitiveComponent *const OtherComponent, int const OtherBodyIndex)
+{
+
 }
