a1f74469520b40bd7db992d62cfab37789e70b0b
tbvjchvkfl / Developer <137769043+tbvjchvkfl@users.noreply.github.com>
2024-06-18 22:09:17 +0900

    코드 정리 및 데이터 구조 변경

--- UE5_HorrorGame/Source/UE5_HorrorsGame/Public/Data/ItemStruct.h
+++ UE5_HorrorGame/Source/UE5_HorrorsGame/Public/Data/ItemStruct.h
@@ -7,43 +7,12 @@
 #include "ItemStruct.generated.h"
 
 UENUM()
-enum class EItemQuality : uint8
-{
-	Shoddy UMETA(DisplayName = "Shoddy"),
-	Common UMETA(DisplayName = "Common"),
-	Quality UMETA(DisplayName = "Quality"),
-	Masterwork UMETA(DisplayName = "Masterwork"),
-	Grandmaster UMETA(DisplayName = "Grandmaster")
-};
-
-UENUM()
 enum class EItemType : uint8
 {
 	Armor UMETA(DisplayName = "Armor"),
 	Weapon UMETA(DisplayName = "Weapon"),
-	Shield UMETA(DisplayName = "Shield"),
-	Spell UMETA(DisplayName = "Spell"),
-	Consumable UMETA(DisplayName = "Consumable"),
-	Quest UMETA(DisplayName = "Quest"),
-	Mundane UMETA(DisplayName = "Mundane")
-};
-
-USTRUCT()
-struct FItemStatistics
-{
-	GENERATED_USTRUCT_BODY()
-
-	UPROPERTY(EditAnywhere)
-	float ArmorRating;
-
-	UPROPERTY(EditAnywhere)
-	float DamageValue;
-
-	UPROPERTY(EditAnywhere)
-	float RestorationAmount;
-
-	UPROPERTY(EditAnywhere)
-	float SellValue;
+	Coin UMETA(DisplayName = "Coin"),
+	Consumable UMETA(DisplayName = "Consumable")
 };
 
 USTRUCT()
@@ -52,28 +21,19 @@ struct FItemTextData
 	GENERATED_USTRUCT_BODY()
 
 	UPROPERTY(EditAnywhere)
-	FText Name;
+	FString Name;
 
 	UPROPERTY(EditAnywhere)
-	FText Description;
-
-	UPROPERTY(EditAnywhere)
-	FText InteractionText;
-
-	UPROPERTY(EditAnywhere)
-	FText UsageText;
+	FString Description;
 };
 
 USTRUCT()
 struct FItemNumericData
 {
 	GENERATED_USTRUCT_BODY()
-
+	
 	UPROPERTY(EditAnywhere)
-	float Weight;
-
-	UPROPERTY(EditAnywhere)
-	int32 maxStackSize;
+	int32 MaxStackSize;
 
 	UPROPERTY(EditAnywhere)
 	bool bIsStackable;
@@ -92,31 +52,30 @@ struct FItemAssetData
 };
 
 
-
-USTRUCT()
+USTRUCT(BlueprintType)
 struct FItemData : public FTableRowBase
 {
 	GENERATED_USTRUCT_BODY()
 
-	UPROPERTY(EditAnywhere)
+	FItemData() : ItemType(EItemType::Consumable)
+	{
+		TextData.Name = "None";
+		TextData.Description = "None";
+	}
+
+	UPROPERTY(EditAnywhere, Category = "ItemData")
 	FName ID;
 
-	UPROPERTY(EditAnywhere)
-	EItemQuality ItemQuality;
-
-	UPROPERTY(EditAnywhere)
+	UPROPERTY(EditAnywhere, Category = "ItemData")
 	EItemType ItemType;
 
-	UPROPERTY(EditAnywhere)
-	FItemStatistics ItemStatistics;
-
-	UPROPERTY(EditAnywhere)
+	UPROPERTY(EditAnywhere, Category = "ItemData")
 	FItemTextData TextData;
 
-	UPROPERTY(EditAnywhere)
+	UPROPERTY(EditAnywhere, Category = "ItemData")
 	FItemNumericData NumericData;
 
-	UPROPERTY(EditAnywhere)
+	UPROPERTY(EditAnywhere, Category = "ItemData")
 	FItemAssetData AssetData;
 };
 
