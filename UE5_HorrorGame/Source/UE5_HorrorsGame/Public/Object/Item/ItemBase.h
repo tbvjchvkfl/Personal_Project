a64f032cf80f57b8ad9b6e4181ac905b4b7008fc
tbvjchvkfl / Developer <137769043+tbvjchvkfl@users.noreply.github.com>
2024-06-23 21:52:18 +0900

    ItemBase 코드 수정 & PickUpItem 추가

--- UE5_HorrorGame/Source/UE5_HorrorsGame/Public/Object/Item/ItemBase.h
+++ UE5_HorrorGame/Source/UE5_HorrorsGame/Public/Object/Item/ItemBase.h
@@ -3,38 +3,29 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "UObject/NoExportTypes.h"
 #include "Data/ItemStruct.h"
 #include "ItemBase.generated.h"
 
-class APlayerCharacter;
-
+/**
+ * 
+ */
 UCLASS()
 class UE5_HORRORSGAME_API UItemBase : public UObject
 {
 	GENERATED_BODY()
+	
 public:
 	// ===========================================================
 	// =                  Variable / Property					 =
 	// ===========================================================
-
-	/*UPROPERTY()
-	UInventoryComponent *OwningInventory;*/
-
 	UPROPERTY(VisibleAnywhere, Category = "ItemData")
-	int32 Quantity;
+	int32 Amount;
 
 	UPROPERTY(VisibleAnywhere, Category = "ItemData")
 	FName ID;
 
 	UPROPERTY(VisibleAnywhere, Category = "ItemData")
-	EItemQuality ItemQuality;
-
-	UPROPERTY(VisibleAnywhere, Category = "ItemData")
 	EItemType ItemType;
-
-	UPROPERTY(VisibleAnywhere, Category = "ItemData")
-	FItemStatistics ItemStatistics;
 
 	UPROPERTY(VisibleAnywhere, Category = "ItemData")
 	FItemTextData TextData;
@@ -45,33 +36,25 @@ public:
 	UPROPERTY(VisibleAnywhere, Category = "ItemData")
 	FItemAssetData AssetData;
 
-
 	// ===========================================================
 	// =					  Functionary	   				     = 
 	// ===========================================================
-
 	UItemBase();
 
 	UItemBase *CreateItemCopy() const;
 
 	UFUNCTION(Category = "Item")
-	FORCEINLINE float GetItemStackWeight() const { return Quantity * NumericData.Weight; }
+	FORCEINLINE bool GetItemStackSize() const { return Amount == NumericData.MaxStackSize; };
 
 	UFUNCTION(Category = "Item")
-	FORCEINLINE float GetItemSingleWeight() const { return NumericData.Weight; }
+	void SetAmount(const int32 NewAmount);
 
 	UFUNCTION(Category = "Item")
-	FORCEINLINE bool IsFullItemStack() const { return Quantity == NumericData.maxStackSize; }
-
-	UFUNCTION(Category = "Item")
-	void SetQuantity(const int32 NewQuantity);
-
-	UFUNCTION(Category = "Item")
-	virtual void Use(APlayerCharacter *Character);
+	virtual void Use(class APlayerCharacter *Character);
 
 protected:
-	bool operator==(const FName &OtherID) const
+	bool operator== (const FName &OtherID)
 	{
-		return ID == OtherID;
+		return this->ID == OtherID;
 	}
 };
