d3044db00a5c43628390802f4576e55ac1b5f733
tbvjchvkfl / Developer <137769043+tbvjchvkfl@users.noreply.github.com>
2024-06-24 22:26:19 +0900

    코드 수정 및 정리

--- UE5_HorrorGame/Source/UE5_HorrorsGame/Public/UI/InGameHUD.h
+++ UE5_HorrorGame/Source/UE5_HorrorsGame/Public/UI/InGameHUD.h
@@ -7,18 +7,33 @@
 #include "InGameHUD.generated.h"
 
 class UTextBlock;
+class UInteractionWidget;
 
 UCLASS()
 class UE5_HORRORSGAME_API UInGameHUD : public UUserWidget
 {
 	GENERATED_BODY()
 	
+public:
+	// ===========================================================
+	// =                  Variable / Property					 =
+	// ===========================================================
 	UPROPERTY(BlueprintReadOnly, Category = "TEXT", meta = (BindWidget, AllowPrivateAccess = "true"))
 	UTextBlock *AmmoCount;
 
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock *CoinText;
 
-public:
-	void Init(int remainAmmoCount, int maxAmmoCount) const;
+	UPROPERTY(meta = (BindWidget))
+	UInteractionWidget *InteractUI;
+
+	// ===========================================================
+	// =					  Functionary	   				     = 
+	// ===========================================================
+	void ShowInteractUI() const;
+	void HideInteractUI()const;
+	void InitializeHUD() const;
 	void SetAmmoCountText(int remain, int max) const;
-	
+	//void SetCoinText(class APlayerCharacter* Player);
 };
+
