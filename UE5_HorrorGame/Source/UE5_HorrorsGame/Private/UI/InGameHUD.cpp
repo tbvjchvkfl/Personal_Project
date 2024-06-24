13fe660a7d6826d97fe7c4496836f6da4eb41a2f
tbvjchvkfl / Developer <137769043+tbvjchvkfl@users.noreply.github.com>
2024-06-24 22:26:43 +0900

    코드 수정 및 정리

--- UE5_HorrorGame/Source/UE5_HorrorsGame/Private/UI/InGameHUD.cpp
+++ UE5_HorrorGame/Source/UE5_HorrorsGame/Private/UI/InGameHUD.cpp
@@ -3,14 +3,42 @@
 
 #include "UI/InGameHUD.h"
 #include "Components/TextBlock.h"
+#include "UI/InteractionWidget.h"
+#include "Kismet/GameplayStatics.h"
+#include "Character/Player/PlayerCharacter.h"
+#include "Armor/Weapon_Pistol.h"
 
-void UInGameHUD::Init(int remainAmmoCount, int maxAmmoCount) const
+void UInGameHUD::ShowInteractUI() const
 {
-	SetAmmoCountText(remainAmmoCount, maxAmmoCount);
+	InteractUI->SetVisibility(ESlateVisibility::Visible);
+}
+
+void UInGameHUD::HideInteractUI() const
+{
+	InteractUI->SetVisibility(ESlateVisibility::Collapsed);
+}
+
+void UInGameHUD::InitializeHUD() const
+{
+	auto Player = Cast<APlayerCharacter>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
+
+	if (auto Pistol = Cast<AWeapon_Pistol>(Player->Pistol))
+	{
+		int AmmoRemain = Pistol ? Pistol->GetCurAmmo() : 0;
+		int AmmoMaxCount = Pistol ? Pistol->GetMaxAmmo() : 0;
+		SetAmmoCountText(AmmoRemain, AmmoMaxCount);
+	}
+	HideInteractUI();
 }
 
 void UInGameHUD::SetAmmoCountText(int remain, int max) const
 {
-	FString string = FString::Printf(TEXT(" %d / %d"), remain, max);
+	FString string = FString::Printf(TEXT("%d / %d"), remain, max);
 	AmmoCount->SetText(FText::FromString(string));
 }
+
+//void UInGameHUD::SetCoinText(class APlayerCharacter *Player)
+//{
+//	FString CoinString = FString::Printf(TEXT("%d"), Player->GetPlayerCoin());
+//	CoinText->SetText(FText::FromString(CoinString));
+//}
