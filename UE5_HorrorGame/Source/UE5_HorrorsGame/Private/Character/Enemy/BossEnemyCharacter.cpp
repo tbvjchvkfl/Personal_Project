38f477ba2d98407ed8d0a0bd14154b6677df971f
tbvjchvkfl / Developer <137769043+tbvjchvkfl@users.noreply.github.com>
2024-07-17 23:42:13 +0900

    보스 캐릭터 추가 (현재 버그 있음)

--- UE5_HorrorGame/Source/UE5_HorrorsGame/Private/Character/Enemy/BossEnemyCharacter.cpp
+++ UE5_HorrorGame/Source/UE5_HorrorsGame/Private/Character/Enemy/BossEnemyCharacter.cpp
@@ -2,4 +2,44 @@
 
 
 #include "Character/Enemy/BossEnemyCharacter.h"
+#include "Character/Player/PlayerCharacter.h"
+#include "Kismet/GameplayStatics.h"
+#include "UI/HorrorsHUD.h"
 
+ABossEnemyCharacter::ABossEnemyCharacter()
+{
+	MaxHealth = 200.0f;
+	CurHealth = MaxHealth;
+}
+
+void ABossEnemyCharacter::BeginPlay()
+{
+	HUD = Cast<AHorrorsHUD>(GetWorld()->GetFirstPlayerController()->GetHUD());
+}
+
+void ABossEnemyCharacter::Tick(float DeltaTime)
+{
+}
+
+void ABossEnemyCharacter::Die(float KillingDamage, FDamageEvent const &DamageEvent, AController *Killer, AActor *DamageCauser)
+{
+}
+
+void ABossEnemyCharacter::MeleeAttackWithSweepTrace()
+{
+	FHitResult HitResult;
+	FVector Start = GetActorLocation();
+	FVector End = GetActorForwardVector() * 200.0f;
+	FCollisionQueryParams Params;
+
+	if (GetWorld()->SweepSingleByChannel(HitResult, Start, End, FQuat::Identity, ECollisionChannel::ECC_GameTraceChannel4, FCollisionShape::MakeSphere(50.0f), Params))
+	{
+		if (HitResult.bBlockingHit)
+		{
+			if (auto Target = Cast<APlayerCharacter>(HitResult.GetActor()))
+			{
+				UGameplayStatics::ApplyDamage(Target, 30.0f, NULL, this, UDamageType::StaticClass());
+			}
+		}
+	}
+}
