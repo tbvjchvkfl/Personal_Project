51b70d737a732929f49266a4e070e2eb79ef2d16
tbvjchvkfl / Developer <137769043+tbvjchvkfl@users.noreply.github.com>
2024-07-20 21:54:07 +0900

    보스 스킬 범위 체크용 서비스 노드 추가

--- UE5_HorrorGame/Source/UE5_HorrorsGame/Public/AI/BT/BTService_IsSkillRange.h
+++ UE5_HorrorGame/Source/UE5_HorrorsGame/Public/AI/BT/BTService_IsSkillRange.h
@@ -6,12 +6,21 @@
 #include "BehaviorTree/Services/BTService_BlackboardBase.h"
 #include "BTService_IsSkillRange.generated.h"
 
-/**
- * 
- */
+
 UCLASS()
 class UE5_HORRORSGAME_API UBTService_IsSkillRange : public UBTService_BlackboardBase
 {
 	GENERATED_BODY()
-	
+public:
+	UBTService_IsSkillRange();
+	void OnBecomeRelevant(UBehaviorTreeComponent &OwnerComp, uint8 *NodeMemory) override;
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI", meta = (AllowPrivateAccess = "true"))
+	float MeleeRange = 500.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI", meta = (AllowPrivateAccess = "true"))
+	float SkillDelay = 10.0f;
+
+	bool IsDoingSkill(ABossEnemyCharacter* BossEnemy, APlayerCharacter* Target);
 };
