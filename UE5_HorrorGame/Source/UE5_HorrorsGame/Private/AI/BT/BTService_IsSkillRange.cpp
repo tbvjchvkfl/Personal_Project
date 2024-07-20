470591c10fa0ceb23895042fc003283152bd6dea
tbvjchvkfl / Developer <137769043+tbvjchvkfl@users.noreply.github.com>
2024-07-20 21:50:17 +0900

    보스 몬스터 스킬 범위 체크용 서비스 노드 추가

--- UE5_HorrorGame/Source/UE5_HorrorsGame/Private/AI/BT/BTService_IsSkillRange.cpp
+++ UE5_HorrorGame/Source/UE5_HorrorsGame/Private/AI/BT/BTService_IsSkillRange.cpp
@@ -2,4 +2,35 @@
 
 
 #include "AI/BT/BTService_IsSkillRange.h"
+#include "AI/BossEnemyController.h"
+#include "Character/Enemy/BossEnemyCharacter.h"
+#include "Character/Player/PlayerCharacter.h"
+#include "Kismet/GameplayStatics.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
+
+UBTService_IsSkillRange::UBTService_IsSkillRange()
+{
+	bNotifyBecomeRelevant = true;
+	NodeName = TEXT("Is In Skill Range");
+}
+
+void UBTService_IsSkillRange::OnBecomeRelevant(UBehaviorTreeComponent &OwnerComp, uint8 *NodeMemory)
+{
+	auto const *const Controller = Cast<ABossEnemyController>(OwnerComp.GetAIOwner());
+
+	auto *const Boss = Cast<ABossEnemyCharacter>(Controller->GetPawn());
+
+	auto *const Player = Cast<APlayerCharacter>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
+
+	OwnerComp.GetBlackboardComponent()->SetValueAsBool(GetSelectedBlackboardKey(), IsDoingSkill(Boss, Player));
+}
+
+bool UBTService_IsSkillRange::IsDoingSkill(ABossEnemyCharacter *BossEnemy, APlayerCharacter *Target)
+{
+	if (BossEnemy->GetDistanceTo(Target) <= MeleeRange && BossEnemy->SkillCoolDown(SkillDelay))
+	{
+		return true;
+	}
+	return false;
+}
