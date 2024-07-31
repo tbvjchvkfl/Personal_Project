9862810e56e62703096044d3699cb76cf05130b0
tbvjchvkfl / Developer <137769043+tbvjchvkfl@users.noreply.github.com>
2024-07-31 22:09:00 +0900

    노티파이 로직 수정 및 코드 정리

--- UE5_HorrorGame/Source/UE5_HorrorsGame/Private/Anim/NotifyState/AttackAnimNotify.cpp
+++ UE5_HorrorGame/Source/UE5_HorrorsGame/Private/Anim/NotifyState/AttackAnimNotify.cpp
@@ -2,4 +2,33 @@
 
 
 #include "Anim/NotifyState/AttackAnimNotify.h"
+#include "Character/Enemy/EnemyCharacter.h"
+#include "Character/Enemy/BossEnemyCharacter.h"
+#include "Kismet/GameplayStatics.h"
 
+void UAttackAnimNotify::OnAnimNotifyCreatedInEditor(FAnimNotifyEvent &ContainingAnimNotifyEvent)
+{
+	// This Function is Initialize for this Notify
+	// 여기서 해당 노티파이를 초기화
+}
+
+void UAttackAnimNotify::Notify(USkeletalMeshComponent *MeshComp, UAnimSequenceBase *Animation)
+{
+	if (MeshComp && MeshComp->GetOwner())
+	{
+		if (auto *NPC = Cast<AEnemyCharacter>(MeshComp->GetOwner()))
+		{
+			NPC->MeleeAttackWithSweepTrace();
+		}
+
+		if (auto *const Boss = Cast<ABossEnemyCharacter>(MeshComp->GetOwner()))
+		{
+			Boss->MeleeAttackWithSweepTrace();
+		}
+	}
+}
+
+//void UAttackAnimNotify::Notify(USkeletalMeshComponent *MeshComp, UAnimSequenceBase *Animation, const FAnimNotifyEventReference &EventReference)
+//{
+//	//위 함수의 확장형
+//}
--- UE5_HorrorGame/Source/UE5_HorrorsGame/Private/Anim/NotifyState/AttackAnimNotifyState.cpp
+++ UE5_HorrorGame/Source/UE5_HorrorsGame/Private/Anim/NotifyState/AttackAnimNotifyState.cpp
@@ -3,25 +3,35 @@
 
 #include "Anim/NotifyState/AttackAnimNotifyState.h"
 #include "Character/Enemy/EnemyCharacter.h"
+#include "Character/Enemy/BossEnemyCharacter.h"
 
 void UAttackAnimNotifyState::NotifyBegin(USkeletalMeshComponent *MeshComp, UAnimSequenceBase *Animation, float TotalDuration)
 {
 	if (MeshComp && MeshComp->GetOwner())
 	{
-		if (AEnemyCharacter *const Character = Cast<AEnemyCharacter>(MeshComp->GetOwner()))
+		/*if (AEnemyCharacter *const Character = Cast<AEnemyCharacter>(MeshComp->GetOwner()))
+		{
+			Character->AttackStart();
+		}*/
+		if (auto const Character = Cast<ABossEnemyCharacter>(MeshComp->GetOwner()))
 		{
 			Character->AttackStart();
 		}
 	}
+	
 }
 
 void UAttackAnimNotifyState::NotifyEnd(USkeletalMeshComponent *MeshComp, UAnimSequenceBase *Animation)
 {
 	if (MeshComp && MeshComp->GetOwner())
 	{
-		if (AEnemyCharacter *const character = Cast<AEnemyCharacter>(MeshComp->GetOwner()))
+		/*if (AEnemyCharacter *const character = Cast<AEnemyCharacter>(MeshComp->GetOwner()))
 		{
 			character->AttackEnd();
+		}*/
+		if (auto const Character = Cast<ABossEnemyCharacter>(MeshComp->GetOwner()))
+		{
+			Character->AttackEnd();
 		}
 	}
 }
