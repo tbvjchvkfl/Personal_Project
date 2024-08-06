6e5b6dc453a2e6074b6a1b1b6858d54499bc0745
tbvjchvkfl / Developer <137769043+tbvjchvkfl@users.noreply.github.com>
2024-08-06 16:07:09 +0900

    데이터 구조체용 파일 코드 정리

---
+++ UE5_HorrorGame/Source/UE5_HorrorsGame/Private/Data/TutorialStruct.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Data/TutorialStruct.h"
+
+TutorialStruct::TutorialStruct()
+{
+}
+
+TutorialStruct::~TutorialStruct()
+{
+}
